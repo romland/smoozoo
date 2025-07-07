c67ad287d2554cad44f53b31195a75ddbb4eed8d
Joakim Romland <romland@gmail.com>
2025-07-07 17:22:16 +0200

    Default example tweaks

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 import { MinimapPlugin } from "../plugins/smoozoo-plugin-minimap.js";
 import { FileChooserPlugin } from "../plugins/smoozoo-plugin-filechooser.js";
-// import { HotspotPlugin } from "../plugins/smoozoo-plugin-hotspot.js";
-// import { OverlayBasePlugin } from "../plugins/smoozoo-plugin-overlay-base.js";
+import { HotspotPlugin } from "../plugins/smoozoo-plugin-hotspot.js";
+import { OverlayBasePlugin } from "../plugins/smoozoo-plugin-overlay-base.js";
 
 window.addEventListener('load', async () => {
     const settings = {
         canvas:                     document.getElementById('smoozoo-glcanvas'),
+        backgroundColor:            "#0e0422",
         initialScale:               0.3,
         initialPosition:            { x: 0.0, y: 0.5 },
         loadingAnimation:           true,
@@ -21,6 +22,35 @@ window.addEventListener('load', async () => {
         dynamicTextureFiltering:    true,   // If greater or less than dynamicFilteringThreshold,
                                             // automatically toggle texture filtering (pixelated or not)
         plugins: [
+            {
+                name: HotspotPlugin,
+                options: {
+                    // Ah, a bit of ugliness to get Parcel to pick up the asset.
+                    hotspots: await (await fetch( new URL(`../assets/ETHUSDT-ath.json`, import.meta.url).toString() )).json(),
+                    objectType: "ATHs"
+                }
+            },
+            {
+                name: OverlayBasePlugin,
+                options: {
+                    hoverOutlineColor: "yellow",
+                    defaultTextFontSize: 14,
+                    defaultTextFontFamily: "sans-serif",
+                    defaultTextFontColor: "#888",
+                    defaultTextBackground: undefined,
+                    // An explicit `null` instead of an array below will show some default test shapes
+                    shapes: [            {
+                        // https://getemoji.com/
+                        type: 'text',
+                        x: 1600, y: 700,
+                        fillStyle: 'yellow',
+                        font: '178px sans-serif',
+                        text: "🧐",
+                        tooltip: "",
+                    },
+                ],
+                }
+            },
             {
                 name: FileChooserPlugin,
                 options: {
